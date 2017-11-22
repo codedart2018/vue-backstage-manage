74bc9a9aefc9588323b0b1882ecabf7c0c7d5f44
myxingke <myxingke@126.com>
2017-11-22 23:54:38 +0800

    上下左右结构

--- src/pages/common/layout.vue
+++ src/pages/common/layout.vue
@@ -1,6 +1,17 @@
+<style src="@/assets/styles/layout/index.less" lang="less" scoped></style>
 <template>
-  <div>
-    <div>this is template body</div>
+  <div class="app-wrapper">
+    <div class="nav-header">头部信息</div>
+    <div class="container">
+      <div class="left-sidebar">
+        左边
+      </div>
+      <div class="right-sidebar">
+        <div style="height: 1500px; border: 1px solid #dedede">
+          this is template body
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
